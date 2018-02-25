f869fa8fd40851fd3b482886a90c09d4e404d9dc
theShivaa <shivkumar201415@gmail.com>
2018-02-25 20:12:07 +0530

    Added Basic ui

--- public/js/index.js
+++ public/js/index.js
@@ -3,9 +3,38 @@ socket.on('connect',function(){
     console.log('Connected to server');
 });
 
+socket.on('newUser',function(newMsg){
+    console.log(newMsg)
+    var li = jQuery('<li></li>');
+    li.text(`${newMsg.from}:${newMsg.text}`);
+
+    jQuery('#messages').append(li);
+})
+
 socket.on('newMessage',function(newMsg){
-    console.log('new messages',newMsg)
+    //console.log('new messages',newMsg)
+    var li = jQuery('<li></li>');
+    li.text(`${newMsg.from}:${newMsg.text}`);
+
+    jQuery('#messages').append(li);
 })
 socket.on('disconnect',function(){
     console.log('Disconnected to server');
 })
+
+// socket.emit('createMessage',{
+//     'From':'Shiva',
+//     'Text':'Hello babua'
+// },function(){
+//     console.log('Got it')
+// })
+
+jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
+    socket.emit('createMessage',{
+        from:'User',
+        text:jQuery('[name=message]').val()
+    },function(){
+        console.log('Hoo')
+    })
+});
--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 const http = require('http');
+
+const {generateMessage} = require('./utils/mesaage')
 const app = express();
 const port = process.env.PORT || 3000 ;
 const publicPath = path.join(__dirname,'../public');
@@ -14,15 +16,19 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New User Connected');
 
-    socket.on('createMessage',(msg)=>{
+    socket.on('createMessage',(msg,callback)=>{
         console.log(msg);
-        io.emit('newMessage',{
+        socket.broadcast.emit('newMessage',{
             'from':msg.from,
             'text':msg.text,
             'createdAt':new Date().getTime()
 
         })
+        callback();
     })
+
+    socket.emit('newUser',generateMessage('Adimn','Welcome to chat'));
+    socket.broadcast.emit('newUser',generateMessage('Admin','New user joined the chat'));
     socket.on('disconnect',(socket)=>{
         console.log('User DisConnected');
     });
